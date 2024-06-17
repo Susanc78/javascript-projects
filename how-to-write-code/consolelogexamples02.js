1eb4893cb38fd48d7311063ddae68bada8882a18
Susan Cox <susancox@Susans-Laptop.local>
2024-06-17 18:32:38 -0500

    chapter 5 exercise

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,15 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+
+
+console.log(typeof "17");
+console.log(typeof "3.2");
+
+
+console.log(Number("2345"));
+console.log(typeof Number("2345"));
+console.log(Number(17));
+
+const appName = "Get It Done";
+appName = "Best TODO application Ever!";
