334bee4d6ea4b27a1509b3a5404367f8368f54a5
Susan Cox <susancox@Susans-Laptop.local>
2024-06-19 10:11:27 -0500

    completed exercises

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,21 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
+// arr=str.split(); - this will return an array with the entire string as one item.
+// arr=str.split('e'); - this will return array with each word as an item with the letter 'e' removed.
+// arr=str.split(''); - returns array with each indifidual letter as an item.
+// arr=str.split(','); - returns array with two items, using the comma to determine the item.
+arr=str.split(' '); //returns array with each index being a separate word
 
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
+//str=arr.join(); - returns array with each word separated by comma
+//str=arr.join('a'); - returns with the letter 'a' connecting each item
+//str=arr.join(''); - no space between items
+str=arr.join(' '); //returns array with space between items
 
 //3) Do split or join change the original string/array?
+//no, they return a new string or array
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+console.log(cargoHold.split(',').sort().join(','));
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -1,10 +1,18 @@
 let holdCabinet1 = ['duct tape', 'gum', 3.14, false, 6.022e23];
 let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
-
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
-
+let holdCabinet3 = holdCabinet1.concat(holdCabinet2);
+console.log(holdCabinet3);
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+console.log(holdCabinet1.slice(2,4));
+console.log(holdCabinet2.slice(0,2));
+//does not alter the original array, simply returns the requested items
 
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse();
+holdCabinet2.sort();
+console.log(holdCabinet1);
+console.log(holdCabinet2);
+//reverse simply inverts the order of items in an array, while sort arranges items in the order acknowledge by javascript.
