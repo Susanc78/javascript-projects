b96dd053673e8f273c65bc5c281b1072dbfed1d7
Susan Cox <susancox@Susans-Laptop.home>
2024-07-16 16:25:29 -0500

    Functions exercises done

--- functions/exercises/exercises.js
+++ functions/exercises/exercises.js
@@ -1,76 +1,98 @@
-function makeLine(size) {               //define the function and parameters
-    let line = "";                      //create variable to hold string of characters
-    for (let i = 0; i < size; i++) {    //write logic for adding characters of the same string together 
-      line += "#";
-    }
-    return line;                        //return the result
-  }                                     //test results
+// Rectangles
 
+// makeLine(size) function
+function makeLine(size) {
+  let line = "";
+  for (let i = 0; i < size; i++) {
+      line += '#';
+  }
+  return line;
+}
+console.log(makeLine(5));
+console.log('');
 
+// makeSquare(size) function
 function makeSquare(size) {
-    let squareString = '';
-    for (let i = 0; i < size; i++) {
-        squareString += makeLine(size);  
-        if (i < size - 1) {             //This is a conditional statement that checks if i is less than size - 1. If it is, it means we are not on the last row of the square, so we need to add a newline.
-            squareString += "\n";       //this adds the newline character to the string
-        }
+  let square = '';
+  for(let i = 0;i < size; i++) {
+      square += (makeLine(size) + '\n');
+  }
+  return square;
+}
+console.log(makeSquare(5));
+console.log('');
+
+// makeRectangle(width, height) function
+function makeRectangle(width, height) {
+  let rectangle = '';
+  for (let i = 0; i < height; i++) {
+    rectangle += (makeLine(width) + '\n');
+  }
+  return rectangle.slice(0, -1);
+}
+console.log(makeRectangle(4,3));
+console.log('');
+
+// Triangles
+
+// makeDownwardStairs(height) function
+function makeDownwardStairs(height) {
+  let stairs = "";
+  for (let i = 1; i <= height; i++) {
+    stairs += makeLine(i+1);
+    if (i < height) {
+      stairs += '\n';
     }
-    return squareString;
+  }
+  return stairs;
+}
+console.log(makeDownwardStairs(5));
+console.log('');
+
+// makeSpaceLine(numSpaces, numChars) function
+function makeSpaceLine(numSpaces, numChars) {
+  let  line = '';
+  for (let i = 0; i < numSpaces; i++) {
+    line += " "
+  }
+  line += makeLine(numChars);
+  for (let i = 0; i < numSpaces; i++) {
+    line += " "
+  }
+  return line; 
 }
 
-function makeRectangle(width, height) {
-    let rectangleString = '';
-    for (let i = 0; i < height; i++) {          //code for each row (height)
-      rectangleString += makeLine(width);          //use a previous functio to add a line
-      if (i < height - 1) {
-        rectangleString += "\n";                 //adding the newline until the last row
+console.log(makeSpaceLine(3, 5));
+console.log('');
+
+// makeIsoscelesTriangle(height) function
+function makeIsoscelesTriangle(height) {
+  let triangle = "";
+  for (let i = 0; i < height; i++) {
+    triangle += makeSpaceLine(height - i - 1, 2 * i + 1);
+    if (i < height - 1) {
+      triangle += '\n';
+    }
+  }
+  return triangle;
+}
+console.log(makeIsoscelesTriangle(5));
+console.log('');
+
+// Diamonds
+
+// makeDiamond(height) function
+function makeDiamond(height) {
+  let diamond = makeIsoscelesTriangle(height);
+  if (height > 1) {
+    diamond += '\n';
+    for (let i = height - 2; i >= 0; i--) {
+      diamond += makeSpaceLine(height - i - 1, 2 * i + 1);
+      if (i > 0) {
+        diamond += '\n';
       }
     }
-    return rectangleString;
   }
-console.log(makeRectangle(5, 3));
-
-function plusTwo(num) {
-  return num + 2;
+  return diamond;
 }
-
-let a = 2;
-
-for (let i=0; i < 4; i++) {
-  a = plusTwo(a);
-}
-
-console.log(a);
-
-function repeater(str) {
-  let repeated = str + str;
-  console.log(repeated);
-}
-
-repeater('Bob');
-
-let num = 42;
-
-function isEven (num) {
-   return num % 2 === 0;
-}
-
-console.log(isEven(43));
-
-let tortoiseOne = {
-  age: 150,
-  species: "Galapagos Tortoise",
-  diet: ["pumpkins", "lettuce", "cabbage"]
-};
-
-let tortoiseTwo = {
-  age: 150,
-  species: "Galapagos Tortoise",
-  diet: ["pumpkins", "lettuce", "cabbage"],
-};
-
-console.log(tortoiseOne === tortoiseTwo);
-
-let num = Math.floor(Math.random()*10);
-
-console.log(num);
+console.log(makeDiamond(5));
